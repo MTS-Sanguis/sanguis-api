5acb53bdfd57f344042a708574c422c63a03e716
Nikolay Murzin <murzin.nikolay@gmail.com>
2017-06-03 15:45:06 +0300

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+rsync -rzvaP -e 'ssh -i ~/.ssh/google_compute_engine' swagger_server sanguis:/tmp/
+
+ssh -i ~/.ssh/google_compute_engine -T sanguis '
+cd /tmp
+cat server.pid | xargs sudo kill
+sudo nohup python -m swagger_server 80 > /tmp/server.out 2> /tmp/server.err < /dev/null & echo $! > server.pid
+'
